f98296bbc8441499c60c53b49f65751d15cc1b37
irwinsnet <stacy.k.irwin@gmail.com>
2021-10-05 08:48:48 -0700

    Added shell script in session 9 for copying files.

---
+++ sessions/s09_files_and_comprehensions/get_files_s9.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+base_url="https://raw.githubusercontent.com/irs1318dev/pyclass_frc/master/sessions/s09_files_and_comprehensions" #
+#
+#echo $base_url
+file="/hopper.txt" #
+echo $base_url$file #
