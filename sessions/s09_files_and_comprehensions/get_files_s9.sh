4d68243013fa5c446c368847f1f1898d5abc5742
irwinsnet <stacy.k.irwin@gmail.com>
2021-10-06 12:30:05 -0700

    Added image of Google Colab security warning to session 02. Fixed bugs in session 09 shell file and notebook.

--- sessions/s09_files_and_comprehensions/get_files_s9.sh
+++ sessions/s09_files_and_comprehensions/get_files_s9.sh
@@ -17,7 +17,7 @@ cd misc_files
 base_url="${base_url}/misc_files"
 wget -nv $base_url/TTY33ASR.jpg
 wget -nv $base_url/YaleNews_hopper-grace.UNIVAC.102635875-CC_0.jpg
-wget -nv $base_url/cwd
+wget -nv $base_url/cwd.py
 wget -nv $base_url/districts.json
 wget -nv $base_url/matches.json
 wget -nv $base_url/temp.json
